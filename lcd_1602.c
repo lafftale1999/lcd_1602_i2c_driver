9866281d26e0a61483ec7e3e1ac35b01d474324b
Carl Broman <lafftale1999@gmail.com>
2025-06-25 16:47:34 +0200

    fixed spelling error

--- lcd_1602.c
+++ lcd_1602.c
@@ -58,7 +58,7 @@ static uint8_t lcd_goto(i2c_master_dev_handle_t handle, uint8_t x, uint8_t y) {
 
 LCD_WRITE_STATUS lcd_1602_send_string(i2c_master_dev_handle_t handle, char *str) {
     clear_screen(handle);
-    goto(0,0);
+    lcd_goto(0,0);
 
     uint8_t char_len = 0;
     uint8_t row = 0;
