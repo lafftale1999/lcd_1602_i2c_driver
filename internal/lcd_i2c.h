37d52470540352f42ddf73010744a7e8bc95fde5
Lafftale <lafftale1999@gmail.com>
2025-08-23 11:28:35 +0200

    updated cmake and i2c

--- internal/lcd_i2c.h
+++ internal/lcd_i2c.h
@@ -12,14 +12,7 @@
 extern "C" {
 #endif
 
-#define I2C_MASTER_SCL_IO           GPIO_NUM_2                  /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           GPIO_NUM_3                  /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              I2C_NUM_0                   /*!< I2C port number for master dev */
-#define I2C_MASTER_FREQ_HZ          100000                      /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
-#define I2C_DEVICE_ADDRESS_LEN      I2C_ADDR_BIT_LEN_7
+#include "i2c_config.h"
 
 /**
  * @brief initializes the i2c communications and
